0c30f25c045c9f379f5b0955b7340db3e3cf9746
NINESTAIRS <jhjzzang14@gmail.com>
2017-11-02 15:40:14 +0900

    17.11.02

--- app/src/main/java/com/keiis/hanjul/admin/DeadlineFragment.java
+++ app/src/main/java/com/keiis/hanjul/admin/DeadlineFragment.java
@@ -42,15 +42,24 @@ public class DeadlineFragment extends Fragment {
     @OnClick(R.id.btn_send_message)
     void sendMessage(){
         //마감 일시 문자 발송
+        sendToMessage();
+
+    }
+
+    //문자 메세지 보내기
+    private void sendToMessage(){
+
+        String numbers[] = {"01040394969", "01089781479"};
 
         SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage("01051502481",null,"대회명 : 충현고등학교 줄넘기 대회\n" +
-                "대회일 : 2017년 12월 16일\n" +
-                "등록마감일 : 2017년 11월 15일\n" +
-                "빠른시간안에 대회등록 부탁 합니다.\n" +
-                "대회 요강은 홈페이지 및 hanjulApp을 통해\n" +
-                "조회 가능 합니다.\n",null,null);
-
+        for(String number : numbers){
+            sms.sendTextMessage(number,null,"대회명 : 충현고등학교 줄넘기 대회\n" +
+                    "대회일 : 2017년 12월 16일\n" +
+                    "등록마감일 : 2017년 11월 15일\n" +
+                    "빠른시간안에 대회등록 부탁 합니다.\n" +
+                    "대회 요강은 홈페이지 및 hanjulApp을 통해\n" +
+                    "조회 가능 합니다.\n",null,null);
+        }
     }
 
     @OnClick(R.id.btn_modify)
@@ -67,26 +76,5 @@ public class DeadlineFragment extends Fragment {
         ButterKnife.bind(this,view);
 
         return view;
-    }
-
-    private void sendSMS(String phoneNumber, String message){
-        String SENT = "SMS_SENT";
-        String DELIVERED = "SMS_DELIVERED";
-
-        PendingIntent sentPI = PendingIntent.getBroadcast(getActivity(),0,new Intent(SENT),0);
-        PendingIntent deliveredPI = PendingIntent.getBroadcast(getActivity(),0,new Intent(DELIVERED),0);
-
-        getActivity().registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                switch (getResultCode()){
-                    case Activity.RESULT_OK:
-                        Toast.makeText(getActivity(),"알림 문자 메시지가 전송되었습니.",Toast.LENGTH_SHORT).show();
-                        break;
-                }
-            }
-        },new IntentFilter(SENT));
-
-
     }
 }
